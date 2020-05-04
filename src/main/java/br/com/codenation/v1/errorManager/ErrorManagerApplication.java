5fce0f5df76bbc5717f4c1811cc5d6fd39a07c1f
llgalvao24 <llgalvao24@gmail.com>
2020-05-03 22:26:12 -0300

    feat(): initial config commit

---
+++ src/main/java/br/com/codenation/v1/errorManager/ErrorManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.codenation.v1.errorManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorManagerApplication.class, args);
+	}
+
+}
