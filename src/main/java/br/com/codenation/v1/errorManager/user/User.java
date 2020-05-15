c4d60b6601bba2692393563df66a411dee850356
llgalvao24 <llgalvao24@gmail.com>
2020-05-15 16:34:12 -0300

    feat(): some improvements & add hasRole method

--- src/main/java/br/com/codenation/v1/errorManager/user/User.java
+++ src/main/java/br/com/codenation/v1/errorManager/user/User.java
@@ -2,12 +2,12 @@ package br.com.codenation.v1.errorManager.user;
 
 import br.com.codenation.v1.errorManager.enums.Profile;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.CollectionTable;
@@ -20,6 +20,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -27,6 +28,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -34,7 +36,6 @@ import java.util.stream.Collectors;
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable, UserDetails {
-
   private static final Long serialVersionUID = 1L;
 
   @Id
@@ -66,19 +67,23 @@ public class User implements Serializable, UserDetails {
   @CollectionTable(name = "PROFILES")
   private Set<Integer> profiles = new HashSet<>();
 
+  @Transient
+  private Collection<? extends GrantedAuthority> authorities;
+
   public User() {
     addProfile(Profile.USER);
     addProfile(Profile.ADMIN);
   }
 
-  public User(String email, String password){
+  public User(String email, String password, Set<Profile> profiles){
+    super();
     this.email = email;
     this.password = password;
-  }
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    this.authorities = profiles
+        .stream()
+        .map(p -> new SimpleGrantedAuthority(
+            p.getDescription())).collect(Collectors.toList()
+        );
   }
 
   public Long getId() {
@@ -98,12 +103,12 @@ public class User implements Serializable, UserDetails {
     this.email = email;
   }
 
-  public void setPassword(String password) {
-    this.password= password;
-  }
-
   public String getPassword() {
     return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password= password;
   }
 
   public Set<Profile> getProfile() {
@@ -114,6 +119,15 @@ public class User implements Serializable, UserDetails {
 
   public void addProfile(Profile profile) {
     profiles.add(profile.getCode());
+  }
+
+  public boolean hasRole(Profile profile) {
+    return getAuthorities().contains(new SimpleGrantedAuthority(profile.getDescription()));
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
   }
 
   @Override
@@ -145,5 +159,18 @@ public class User implements Serializable, UserDetails {
         ", createdAt=" + createdAt +
         ", updatedAt=" + updatedAt +
         '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof User)) return false;
+    User user = (User) o;
+    return Objects.equals(getId(), user.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
   }
 }
