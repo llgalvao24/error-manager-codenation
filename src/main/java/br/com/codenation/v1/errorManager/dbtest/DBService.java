c3cabc4716f37af1359cb9f6979955f82d995d8c
llgalvao24 <llgalvao24@gmail.com>
2020-05-16 09:39:37 -0300

    feat(): add authorization

---
+++ src/main/java/br/com/codenation/v1/errorManager/dbtest/DBService.java
@@ -0,0 +1,23 @@
+package br.com.codenation.v1.errorManager.dbtest;
+
+import br.com.codenation.v1.errorManager.user.User;
+import br.com.codenation.v1.errorManager.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+
+  @Autowired
+  private BCryptPasswordEncoder passwordEncoder;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  public void instantiateTestDatabase() {
+
+    User user1 = new User("admin@admin.com", passwordEncoder.encode("admin"));
+    userRepository.save(user1);
+  }
+}
