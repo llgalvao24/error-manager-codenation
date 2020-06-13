1a51a5be1e2db195c54c0af7ac8024b3831b0f8e
grsantos13 <grodrigueset@gmail.com>
2020-06-13 15:29:27 -0300

    Adaptado a criação da trigger para a linguagem aceita pelo postgreSQL;

---
+++ src/main/resources/db/migration/V1.2__Created_some_indexes_and_trigger.sql
@@ -0,0 +1,26 @@
+CREATE INDEX IDX_USERS_USERNAME ON USERS (lower(USERNAME));
+
+CREATE INDEX IDX_APPLICATIONS_NAME ON APPLICATIONS (lower(NAME));
+
+CREATE INDEX IDX_PROFILES_USER_ID ON PROFILES (USER_ID);
+
+<!--criação da trigger-->
+
+CREATE OR REPLACE FUNCTION update_user_to_false()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+	IF NEW.is_active <> OLD.is_active AND NEW.is_active = false THEN
+		 UPDATE applications SET is_active = false WHERE user_id = NEW.id;
+	END IF;
+
+	RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE TRIGGER TRG_UPDATE_USER_TO_FALSE
+  BEFORE UPDATE
+  ON USERS
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_user_to_false();
